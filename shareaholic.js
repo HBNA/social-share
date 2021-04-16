3d4efc0955014d9819f98799e616bbaa6f12e02f
HBNA <82637373+HBNA@users.noreply.github.com>
2021-04-15 23:30:25 -0700

    Create shareaholic.js

---
+++ shareaholic.js
@@ -0,0 +1,5 @@
+<!-- BEGIN SHAREAHOLIC CODE -->
+<link rel="preload" href="https://cdn.shareaholic.net/assets/pub/shareaholic.js" as="script" />
+<meta name="shareaholic:site_id" content="8401ae3cc1f15fb104607f7bfedc9b93" />
+<script data-cfasync="false" async src="https://cdn.shareaholic.net/assets/pub/shareaholic.js"></script>
+<!-- END SHAREAHOLIC CODE -->
